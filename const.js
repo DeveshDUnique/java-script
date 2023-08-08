901b9287e24d3b900a5472e5e7e3bfb6e87b4e16
Devesh Patil <75167794+deveshpatil619@users.noreply.github.com>
2023-08-08 10:19:51 +0530

    Add files via upload

---
+++ const.js
@@ -0,0 +1,28 @@
+let radius = 5
+const pi= 3.14  // constant variables cannot be changed
+let area
+
+//pi = 7.2  constant variables cannot be changed
+
+area = pi * radius* radius
+
+console.log(area)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ datatypes.js
@@ -0,0 +1,111 @@
+let num = 8
+
+
+
+let f = 7.8
+
+
+console.log(f)
+console.log(typeof f) //number type all integers and floats
+
+let b = 0xf  // hexadecimal
+let y = 0xff
+console.log(y)
+
+
+let o = 100_000_00 // underscores for our understanding
+console.log(o)
+
+
+let u = 6/0   // infinity
+console.log(u)
+
+console.log(Number.MAX_VALUE)
+console.log(Number.MAX_VALUE * 10)
+
+
+let big = 4555579636245577895656n   //big-int is represented by adding n at the end of number
+console.log(big)
+
+
+// if we want to add number to bigint we need to convert the no to bigint
+
+let big1 = 4555579636245577895656n
+console.log(big1 * 33n)
+
+
+
+let firstname = "Dev"  //string
+
+let lastname = "patil"
+
+
+let user = firstname + " " + lastname  // concatinating two strings
+
+
+console.log(user)
+
+
+
+let username = 'dev patil "cs"' // giving cs in double quotes.
+console.log(username)
+
+
+
+//   escape character //
+
+// forward slash  \\ 
+let gds = "devesh patil \"cs\" "
+console.log(gds)
+
+// \n for new line,   \t for tab space
+
+
+
+//undefined in javascript
+
+let bhs 
+console.log(bhs)
+
+// null is object in js
+
+
+let yiu = null
+console.log(yiu)
+console.log(typeof(yiu)) // object
+
+
+
+console.log(num/"dev")  //nan
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
