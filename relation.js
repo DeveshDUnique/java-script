a7fda86b820d545c761ffa3abb0b746c0f8b7830
deveshpatil619 <deveshpatil619@gmail.com>
2023-08-09 19:03:05 +0530

    updates

---
+++ relation.js
@@ -0,0 +1,25 @@
+console.log(5>6) //false
+
+console.log(9>8) //true
+
+
+//comparing strings
+
+
+let x = "pen"
+let y ="book"
+
+let data = x > y; // it will compare character by character and p is afterwards than b in dictionary so p>b
+console.log(data)
+
+
+
+let u = "2"
+let v = 1
+
+let com = u > v //true as it will convert the string u into numbers
+console.log(com)
+
+
+
+
