7c45e4e21ead3fa8e8bdb0fe6d7eb9492831d698
deveshpatil619 <deveshpatil619@gmail.com>
2023-08-14 18:17:27 +0530

    updates

---
+++ methods_in_objects.js
@@ -0,0 +1,25 @@
+let laptop =
+{
+    cpu : 'i5',
+    ram : '16gb',
+    brand : 'asus',
+
+
+    greet :function()
+    {
+        console.log("method in object")
+    }
+}
+
+laptop.greet()
+
+
+
+
+
+
+
+
+
+
+
