4cc6c245841d5370755b2d62cdba31ad9c29ef53
deveshpatil619 <deveshpatil619@gmail.com>
2023-08-15 17:40:45 +0530

    updates

---
+++ array.js
@@ -0,0 +1,23 @@
+let arr = [];  //empty array
+
+let val = [3,2,1,6,7]
+
+console.log(val,val.length)
+
+
+val.push(4); // adding values in array
+
+console.log(val)
+
+
+console.log(val[3])
+
+
+
+
+
+
+
+
+
+
